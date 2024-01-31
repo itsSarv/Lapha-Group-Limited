8179b737e5aac47b422d87d123ac5dbf62189f61
saurav tiwari <sauravtiwari145@gmail.com>
2024-01-31 23:06:44 +0545

    adding nav-bar

---
+++ client/src/components/login/page.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Image from "next/image";
+
+const login = () => {
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
+        <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+          <Image src="/laphagroup.png" width="100" height="100" />
+          <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
+            Lapha Group Limited
+          </h2>
+          <div classNameName="relative mb-4">
+            <label for="email" className="leading-7 text-sm text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label for="Password" className="leading-7 text-sm text-gray-600">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <button className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg relative mb-2">
+            Login
+          </button>
+          <button className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
+            Register
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default login;
