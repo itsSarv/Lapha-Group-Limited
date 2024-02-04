71295e535ec5ab1a1dcb6c77798e82b59d4d13be
saurav tiwari <sauravtiwari145@gmail.com>
2024-02-04 23:37:04 +0545

    Small tailwind changes on register & login page

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -5,14 +5,14 @@ import Link from "next/link";
 const login = () => {
   return (
     <section className="text-gray-600 body-font">
-      <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
-        <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
-          <Image src="/laphagroup.png" width="100" height="100" />
-          <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
+      <div className="container px-5 py-24 mx-auto flex flex-wrap items-center" >
+        <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 m-auto">
+          <div className= "m-auto"><Image src="/laphagroup.png" width="100" height="100"/></div>
+          <h2 className="text-gray-900 text-lg font-medium title-font mb-5 m-auto">
             Lapha Group Limited
           </h2>
-          <div classNameName="relative mb-4">
-            <label for="email" className="leading-7 text-sm text-gray-600">
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
               Email
             </label>
             <input
@@ -23,7 +23,7 @@ const login = () => {
             />
           </div>
           <div className="relative mb-4">
-            <label for="Password" className="leading-7 text-sm text-gray-600">
+            <label htmlFor="Password" className="leading-7 text-sm text-gray-600">
               Password
             </label>
             <input
@@ -38,7 +38,8 @@ const login = () => {
           </button>
           <Link href="/register"><button className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
             Register
-          </button></Link>
+          </button>
+          </Link>
         </div>
       </div>
     </section>
--- client/src/app/register/page.js
+++ client/src/app/register/page.js
@@ -6,49 +6,49 @@ import Nav from "@/components/navBar/page";
 
 
 
-const login = () => {
+const register = () => {
   return (
     <section className="text-gray-600 body-font">
-           <Nav />
-      <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
-        <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
-          <Image src="/laphagroup.png" width="100" height="100" />
-          <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
-            Lapha Group Limited
-          </h2>
-          <div className="relative mb-4">
-            <label for="email" className="leading-7 text-sm text-gray-600">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <div className="relative mb-4">
-            <label for="Password" className="leading-7 text-sm text-gray-600">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <button className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg relative mb-2">
-            Register
-          </button>
-       <Link href="/"><button className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
-            Back
-          </button>
-          </Link>
+      <Nav/>
+    <div className="container px-5 py-24 mx-auto flex flex-wrap items-center" >
+      <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 m-auto">
+        <div className= "m-auto"><Image src="/laphagroup.png" width="100" height="100"/></div>
+        <h2 className="text-gray-900 text-lg font-medium title-font mb-5 m-auto">
+          Lapha Group Limited
+        </h2>
+        <div className="relative mb-4">
+          <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
         </div>
+        <div className="relative mb-4">
+          <label htmlFor="Password" className="leading-7 text-sm text-gray-600">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
+        </div>
+        <button className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg relative mb-2">
+          Login
+        </button>
+        <Link href="/"><button className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
+          Login
+        </button>
+        </Link>
       </div>
-    </section>
+    </div>
+  </section>
   );
 };
 
-export default login;
+export default register;
